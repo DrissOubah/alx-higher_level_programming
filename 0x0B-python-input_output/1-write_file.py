81464f646688051404f065dee3dd0c719da38522
DrissOubah <drissoubahmane@gmail.com>
2023-10-10 17:06:52 -0700

    WRite File utf - 8 instead of utf=8

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ Contains the function "wrtie_file"
 
 def write_file(filename="", text=""):
     """returns the number of charachters written to filename from text """
-    with open(filename, 'w', encoding='utf=8') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
