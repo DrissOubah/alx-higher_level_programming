90cdb212c79e198d69e03cc521ab9911fd113e0d
DrissOubah <drissoubahmane@gmail.com>
2023-10-10 17:55:25 -0700

    11-Students

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Contains the clas "Student"
+"""
+
+
+class Student:
+    """Representation of a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.last_name = last_name
+        self.first_name = first_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary representation
+        of a Student instance with specified attributes"""
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for x in attrs:
+            try:
+                new_dict[x] = self.__dict__[x]
+            except KeyError:
+                pass
+        return new_dict
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key in json:
+            try:
+                setattr(self, key, json[key])
+            except KeyError:
+                pass
