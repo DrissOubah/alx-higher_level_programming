934d379ff440b4b0ae68682edf49f335173490f6
DrissOubah <drissoubahmane@gmail.com>
2023-10-10 18:05:30 -0700

    7 aDD

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-"""Program to save strings from command
-line arguments to a file called add_item.json.
-"""
+"""Add all arguments to a Python list and save them to a file."""
+import sys
 
-if __name__ == "__main":
-    import sys
-    import json
-    from os.path import isfile
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-    s_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    l_f_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-    filename = "add_item.json"
-
-    if isfile(filename):
-        file_data = l_f_json_file(filename)
-    else:
-        file_data = []
-
-    if len(sys.argv) > 1:
-        file_data.extend(sys.argv[1:])
-
-    s_to_json_file(file_data, filename)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
