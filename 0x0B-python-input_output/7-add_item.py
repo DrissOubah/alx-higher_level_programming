d6b6376e8ea271659babdfd37514c71a69b67da0
DrissOubah <drissoubahmane@gmail.com>
2023-10-10 17:59:23 -0700

    Add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""Program to save strings from command line arguments to file called
-add_item.json.
+"""Program to save strings from command line arguments to a file called add_item.json.
 """
 
-if __name__ == "__main__":
+if __name__ == "__main":
     import sys
     import json
+    from os.path import isfile
 
-    save_to_json_file = \
-        __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     filename = "add_item.json"
-    with open(filename, 'a+') as f:
-        if f.tell() == 0:
-            json.dump([], f)
-    file_data = load_from_json_file("add_item.json")
+
+    if isfile(filename):
+        file_data = load_from_json_file(filename)
+    else:
+        file_data = []
+
     if len(sys.argv) > 1:
         file_data.extend(sys.argv[1:])
+
     save_to_json_file(file_data, filename)
