e0e988db028d45e09e38cae4f0b233ff0ff8c06d
drissOubah <drissoubahmane@gmail.com>
2023-11-15 05:57:01 -0800

    Average Temperature

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- AVERAGE temeprature by city you find THE city in line 26 and balue line 30
-SELECT city ,AVG('value') AS avg_temp FROM temperature GROUP BY 'city' ORDER BY avg_temp DESC;
+SELECT city ,AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
