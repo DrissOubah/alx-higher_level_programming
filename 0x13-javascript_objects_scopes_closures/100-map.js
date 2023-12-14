170e071cbc3f13f34004bf929fd8926c7b032450
drissoubah <drissoubahmane@gmail.com>
2023-12-13 16:38:32 -0800

    Mapping Inside a list using Value and Index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,6 +2,6 @@
 const listData = require('./100-data.js');
 const list = listData.list;
 console.log(list);
-const map1 = list.map((value,index) => value * index);
+const map1 = list.map((value, index) => value * index);
 
 console.log(map1);
