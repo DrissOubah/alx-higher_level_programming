d602ba62ce81ecc977146e00fcb689d968e2f87c
drissOubah <drissoubahmane@gmail.com>
2023-12-17 17:14:08 -0800

    1. Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    """
+    Get MySQL username, password, and database
+    name from command line arguments
+    """
+    username, password, dbname = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    """ Connect to the MySQL server running on localhost at port 3306"""
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=dbname
+            )
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
+        ORDER BY states.id ASC")
+    table = c.fetchall()
+
+    for row in table:
+        print(row)
+
+    c.close()
+    db.close()
