9c6317101e587735b9f0331e3d96b97823d90d41
drissOubah <drissoubahmane@gmail.com>
2023-12-17 17:50:48 -0800

    Cities by state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""script that lists all cities from the database hbtn_0e_4_usa"""
+"""
+Script that lists all cities from the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
 
 if __name__ == '__main__':
-    import MySQLdb
-    import sys
+    """ Get MySQL username, password, and database name from
+    command line arguments"""
+    username, password, dbname = sys.argv[1:4]
 
-    """
-    Get MySQL username, password, and database
-    name from command line arguments
-    """
-    username, password, dbname = sys.argv[1], sys.argv[2], sys.argv[3]
-    state_name = sys.argv[4]
     """ Connect to the MySQL server running on localhost at port 3306"""
     db = MySQLdb.connect(
             host='localhost',
@@ -21,11 +20,17 @@ if __name__ == '__main__':
             )
 
     c = db.cursor()
-    c.execute("SELECT cities.id, cities.name, states.name FROM cities\
-    JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC")
-    table = c.fetchall()
 
-    for row in table:
+    c.execute("""
+              SELECT cities.id, cities.name, states.name
+              FROM cities
+              JOIN states ON states.id = cities.state_id
+              ORDER BY cities.id ASC
+              """)
+
+    rows = c.fetchall()
+
+    for row in rows:
         print(row)
 
     c.close()
