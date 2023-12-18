f2886d25e8448da16aef80b6381d8d2a41d383ea
drissOubah <drissoubahmane@gmail.com>
2023-12-17 17:47:46 -0800

    5. All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
 script that takes in the name of a state as an argument
-and lists all cities of that state, using the database hbtn_0e_4_us
-a"""
+and lists all cities of that state, using the database hbtn_0e_4_usa
+"""
 if __name__ == '__main__':
     import MySQLdb
     import sys
@@ -11,8 +11,7 @@ if __name__ == '__main__':
     Get MySQL username, password, and database
     name from command line arguments
     """
-    username, password, dbname = sys.argv[1], sys.argv[2], sys.argv[3]
-    state_name = sys.argv[4]
+    username, password, dbname, state_name = sys.argv[1:5]
     """ Connect to the MySQL server running on localhost at port 3306"""
     db = MySQLdb.connect(
             host='localhost',
@@ -23,19 +22,15 @@ if __name__ == '__main__':
             )
 
     c = db.cursor()
-   c.execute("SELECT cities.name FROM cities\
-                 LEFT JOIN states\
-                 ON states.id = cities.state_id\
-                 WHERE states.name LIKE BINARY (%s) ORDER BY cities.id ASC",
-                (state_name,))
-    table = c.fetchall()
-
-    end_string = ""
-    string_cities = ""
-    for row in table:
-        string_cities = string_cities + end_string + row[0]
-        end_string = ", "
-
-    print(string_cities)
+    c.execute("""
+              SELECT cities.name
+              FROM cities
+              LEFT JOIN states ON states.id = cities.state_id
+              WHERE states.name LIKE BINARY %s
+              ORDER BY cities.id ASC
+              """, (state_name,))
+    rows = c.fetchall()
+    city_names = ", ".join(row[0] for row in rows)
+    print(city_names)
     c.close()
     db.close()
