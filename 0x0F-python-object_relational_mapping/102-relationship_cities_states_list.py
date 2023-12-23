ce11e8ccb140f29948808ef469d534772ce18b45
drissOubah <drissoubahmane@gmail.com>
2023-12-22 17:44:19 -0800

    From city

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,19 +1,43 @@
 #!/usr/bin/python3
 """
-Lists all City objects from the database hbtn_0e_101_usa.
+Script that lists all City objects from the database hbtn_0e_101_usa.
 """
-import sys
+
+from sys import argv
+from model_state import Base, State
+from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import State
-from relationship_city import City
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    """
+    Lists all City objects from the database.
+    """
+
+    # Get command-line arguments
+    username, password, database_name = argv[1], argv[2], argv[3]
+
+    # Construct the database URI
+    db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format
+    (username, password, database_name)
+
+    # Create an SQLAlchemy engine
+    engine = create_engine(db)
+
+    # Create a session factory
     Session = sessionmaker(bind=engine)
+
+    # Create a session
     session = Session()
 
-    for city in session.query(City).order_by(City.id):
-        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+    try:
+        cities = session.query(City).order_by(City.id).all()
+
+        # Display the results
+        for city in cities:
+            print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+
+    finally:
+        # Close the session
+        if session:
+            session.close()
