1e669549f92ea460859c759f26693e4034f93e16
drissOubah <drissoubahmane@gmail.com>
2024-03-12 19:27:04 -0700

    6 square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const LastSquare = require('./5-square');
+
 class Square extends LastSquare {
-	charPrint (c) {
-		if (typeof c !== 'string' || c.length !== 1) {
+  charPrint (c) {
+    if (typeof c !== 'string' || c.length !== 1) {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
