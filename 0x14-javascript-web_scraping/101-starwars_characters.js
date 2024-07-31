f44a7bd5a08055d90fd0cf6d3376478f357bf3f7
DrissOubah <drissoubahmane@gmail.com>
2024-07-30 18:51:13 -0700

    101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,27 @@
 #!/usr/bin/node
 
-const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+const request = require('request'); // Import the 'request' module for making HTTP requests
+const id = process.argv[2]; // Get the movie ID from the command-line arguments
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`; // Construct the URL for the specified movie
 
 request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
-    }
-  }
+	if (error) {
+		console.log(error); // Log any errors that occur during the request
+		} else {
+			const content = JSON.parse(body); // Parse the response body as JSON
+			const characters = content.characters; // Extract the list of character URLs from the movie data
+			    
+			// Loop through each character URL
+			for (const character of characters) {
+				// Make a GET request to each character URL
+				request.get(character, (error, response, body) => {
+					if (error) {
+						console.log(error); // Log any errors that occur during the request
+						} else {
+							const names = JSON.parse(body); // Parse the response body as JSON
+							console.log(names.name); // Print the name of the character
+							}
+					});
+				}
+			}
 });
