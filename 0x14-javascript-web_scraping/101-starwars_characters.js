0a1c2dce5f460d426064d75bf4a2e5aa3ed9d4cd
DrissOubah <125615597+DrissOubah@users.noreply.github.com>
2024-07-31 03:04:42 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,26 +1,24 @@
 #!/usr/bin/node
 // Script that prints all characters of a Star Wars movie
 
-const request = require('request'); // Import the 'request' module for making HTTP requests
-const id = process.argv[2]; // Get the movie ID from the command-line arguments
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`; // Construct the URL for the specified movie
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request.get(url, (error, response, body) => {
 	if (error) {
-		console.log(error); // Log any errors that occur during the request
+		console.log(error);
 		} else {
-			const content = JSON.parse(body); // Parse the response body as JSON
-			const characters = content.characters; // Extract the list of character URLs from the movie data
-			    
-			// Loop through each character URL
+			const content = JSON.parse(body);
+			const characters = content.characters;
+
 			for (const character of characters) {
-				// Make a GET request to each character URL
 				request.get(character, (error, response, body) => {
 					if (error) {
-						console.log(error); // Log any errors that occur during the request
+						console.log(error);
 						} else {
-							const names = JSON.parse(body); // Parse the response body as JSON
-							console.log(names.name); // Print the name of the character
+							const names = JSON.parse(body);
+							console.log(names.name);
 							}
 					});
 				}
